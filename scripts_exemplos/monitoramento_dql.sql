c83cf0ed164847e70af28bd8398991f5c9548966
Alice <120612012+AliceFonseca15@users.noreply.github.com>
2025-12-16 17:32:35 +0000

    testando novas estruturas

--- scripts_exemplos/monitoramento_dql.sql
+++ scripts_exemplos/monitoramento_dql.sql
@@ -2,6 +2,37 @@ select concat(nome,'-',estado) as "Cidade_estado" from dbo.cidade;
 
 select latitude,longitude from dbo.localizacao;
 
+select distinct nome,estado
+    from dbo.cidade;
+
+select nome,estado
+    from dbo.cidade
+    order by estado asc, nome asc;
+
+select estado, count(*) as quantidade
+    from dbo.cidade
+    group by estado
+
+select estado, count(*) as quantidade
+    from dbo.cidade
+    group by estado
+    having estado in('BA','RN');
+
+insert into dbo.cidade(nome,estado) VALUES
+    ('Natal','RN');
+
+select tempo_max,tempo_min
+    from dbo.tempo
+    where id_cidade in (
+        select id
+            from dbo.cidade
+            where estado = 'BA'
+    ); 
+
+select id 
+    from dbo.cidade
+    where estado = 'BA';
+
 select * from dbo.tempo;
 
 select * from dbo.central;
